9894cc9f7132231178d6f19e0fe0a998063fa792
hjavoras <miniprogrammerme@gmail.com>
2015-11-07 20:50:06 +0100

    missing files

---
+++ OnliStam.Pomocnici/Parametri.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnliStam.Pomocnici
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    sealed class ParametriAttribute: Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly string[] _parametri;
+
+        // This is a positional argument
+        public ParametriAttribute(params string[] parametri)
+        {
+            this._parametri = parametri;
+
+            // TODO: Implement code here
+            throw new NotImplementedException();
+        }
+
+        public string[] Parametri
+        {
+            get { return _parametri; }
+        }
+
+        // This is a named argument
+        public int NamedInt { get; set; }
+    }
+}
---
+++ Online Stamparija/Controllers/AccountController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Stamparija.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Account/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Account/Details/5
+        public ActionResult Login(Models.Korisnik model)
+        {
+            Models.LogovaniKorisnik.Instanca.Login(model.UserName, model.Password);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Logout(Models.Korisnik model)
+        {
+            Models.LogovaniKorisnik.Instanca.LogOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: Account/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Account/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
