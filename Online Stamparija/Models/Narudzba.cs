551ce29846a4ce137ce23d106bb1771518e778f7
Dzenana <dmahmutspa1@etf.unsa.ba>
2016-12-04 23:13:32 +0100

    Dodavanje modela Narudzba

---
+++ Online Stamparija/Models/Narudzba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Online_Stamparija.Models
+{
+    public class Narudzba
+    {
+        public int ID { get; set; } //mora imati ID :)
+
+        public string NazivKorisnika { get; set; } //ime prezime ili firma
+
+        public string Adresa { get; set; }
+
+        public string BrojTelefona { get; set; }
+
+        public string Opis { get; set; }
+
+        public string Napomena { get; set; }
+
+        public int brojKomada { get; set; }
+
+        public DateTime vrijemeIsporuke { get; set; }
+
+        public string Status { get; set; }
+
+        public string NazivFajla { get; set; }
+
+        public string usernameKorisnika { get; set; }
+        //Komentar
+    }
+}
